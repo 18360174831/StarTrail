f7fef834e32a35eef64616644606c0963b383fd9
18360174831 <75061082+18360174831@users.noreply.github.com>
2026-05-28 03:03:48 +0800

    fix: register去掉nickname参数

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -3,8 +3,8 @@ import request from './request'
 export const login = (username: string, password: string) =>
   request.post('/auth/login', { username, password })
 
-export const register = (username: string, password: string, nickname: string) =>
-  request.post('/auth/register', { username, password, confirmPassword: password, nickname })
+export const register = (username: string, password: string) =>
+  request.post('/auth/register', { username, password, confirmPassword: password, nickname: username })
 
 export const getProfile = () => request.get('/auth/me')
 
